99d23c923876603b33d76310de97ff2c2fb14e15
Leanne251 <leannesmith251@gmail.com>
2021-11-27 13:24:12 +0000

    task 2

--- task-2/main.js
+++ task-2/main.js
@@ -1,1 +1,27 @@
 
+
+let counter = document.querySelector("p");
+count = 0;
+
+function increaseTime(){
+    counter.innerText = count++;
+    if (count === 13){
+        clearInterval(countToTwelve);
+    } 
+}
+
+let countToTwelve = setInterval(increaseTime, 1000)
+
+
+// create an element for the counter using querySelecter
+
+//counter is an object.
+// every loop i want to add one to counter. 
+
+
+// use setInterval to count to 12 seconds. .. set Interval takes a function & a time in miliseconds.
+// create a function to use in setInterval which will add one to counter 
+//every second. 
+
+//use clearTimeout to cancel timer. 
+
